f3d76ef8b626aad516b20dc5a305ab1a313d8979
Kamalmeherremov12344321 <ayshenmeherremova90@gmail.com>
2025-10-09 19:38:18 +0400

    ders90 ppart2

--- OKT17/java script/ders90/eeerrrrorrr.js
+++ OKT17/java script/ders90/eeerrrrorrr.js
@@ -1,18 +1,1 @@
-let masiv = [12,11,44,55,65]
-let copymasiv = masiv
-console.log(copymasiv);
 
-let kka = copymasiv.length()
-let hsh = kka[0]
-let hshi = hsh + hsh
-console.log(hshi);
-
-
-let masiiiiihnn = [232,728,88,2399902,938399292]
-let mm = Math.max(...masiiiiihnn)
-console.log(mm);
-
-
-let masiiiiihn = [232,728,88,2399902,938399292]
-let mn = Math.min(...masiiiiihn)
-console.log(mn);
